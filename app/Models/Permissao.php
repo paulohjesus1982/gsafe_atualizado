a24b2707d63053d98ab1c6a2a3a2a60026da41ff
GabrielMoreira <gabrielc.moreira97@gmail.com>
2022-12-03 11:22:28 -0300

    Salvando alterações // CRUD Permissões funcionando

--- app/Models/Permissao.php
+++ app/Models/Permissao.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Permissao extends Model
 {
     use HasFactory;
+
+    protected $table = 'permissoes';
+    protected $primaryKey = 'per_id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'per_id',
+        'per_nome',
+        'per_rgb'
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use App\Http\Controllers\EquipeController;
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\PermissaoController;
 
 Route::get('/', function () {
     return view('/auth/login');
@@ -15,18 +16,27 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 // Usuario
 Route::group(['prefix' => '/usuario'], function () {
-    Route::get('/listar', [UsuarioController::class, 'Listar'])->name('usuario.listar');
-    Route::get('/cadastrar', [UsuarioController::class, 'Cadastrar']);
-    Route::get('/editar/{id}', [UsuarioController::class, 'Editar'])->name('usuario.editar');
-    Route::post('/atualizar', [UsuarioController::class, 'Atualizar'])->name('usuario.atualizar');
-    Route::post('/salvar', [UsuarioController::class, 'Salvar'])->name('usuario.salvar');
+    Route::get('/listar',      [UsuarioController::class, 'Listar']      )->name('usuario.listar');
+    Route::get('/cadastrar',   [UsuarioController::class, 'Cadastrar']   );
+    Route::get('/editar/{id}', [UsuarioController::class, 'Editar']      )->name('usuario.editar');
+    Route::post('/atualizar',  [UsuarioController::class, 'Atualizar']   )->name('usuario.atualizar');
+    Route::post('/salvar',     [UsuarioController::class, 'Salvar']      )->name('usuario.salvar');
 });
 
 // Equipe
 Route::group(['prefix' => '/equipe'], function () {
-    Route::get('/listar', [EquipeController::class, 'Listar'])->name('equipes.listar');
-    Route::get('/cadastrar', [EquipeController::class, 'Cadastrar']);
-    Route::post('/salvar', [EquipeController::class, 'Salvar'])->name('equipes.salvar');
-    Route::post('/atualizar', [EquipeController::class, 'Atualizar'])->name('equipes.atualizar');
-    Route::get('/editar/{id}', [EquipeController::class, 'Editar'])->name('equipes.editar');
+    Route::get('/listar',      [EquipeController::class, 'Listar']      )->name('equipes.listar');
+    Route::get('/cadastrar',   [EquipeController::class, 'Cadastrar']   );
+    Route::post('/salvar',     [EquipeController::class, 'Salvar']      )->name('equipes.salvar');
+    Route::post('/atualizar',  [EquipeController::class, 'Atualizar']   )->name('equipes.atualizar');
+    Route::get('/editar/{id}', [EquipeController::class, 'Editar']      )->name('equipes.editar');
+});
+
+// Permissao
+Route::group(['prefix' => '/permissao'], function () {
+    Route::get('/listar',       [PermissaoController::class, 'Listar']     )->name('permissao.listar');
+    Route::get('/cadastrar',    [PermissaoController::class, 'Cadastrar']  );
+    Route::post('/salvar',      [PermissaoController::class, 'Salvar']     )->name('permissao.salvar');
+    Route::post('/atualizar',   [PermissaoController::class, 'Atualizar']  )->name('permissao.atualizar');
+    Route::get('/editar/{id}',  [PermissaoController::class, 'Editar']     )->name('permissao.editar');
 });
