85594487ba0dc9f5db27b7edfc26229275007838
marioaquicob <mario.acerteaqui@gmail.com>
2023-01-09 16:16:32 -0300

    ajustando href errado

--- resources/views/contrato/editar.blade.php
+++ resources/views/contrato/editar.blade.php
@@ -14,7 +14,7 @@
               <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                   <li class="breadcrumb-item"><a href="/home">Home</a></li>
-                  <li class="breadcrumb-item"><a href="/empresa/listar">Listar</a></li>
+                  <li class="breadcrumb-item"><a href="/contrato/listar">Listar</a></li>
                   <li class="breadcrumb-item active">Editar</li>
                 </ol>
               </div>
--- resources/views/empresa/listar.blade.php
+++ resources/views/empresa/listar.blade.php
@@ -26,7 +26,7 @@
         <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="/home">Home</a></li>
-            <li class="breadcrumb-item"><a href="/empresa/listar">Listar</a></li>
+            <li class="breadcrumb-item active">Listar</li>
         </ol>
         </div>
     </div>
